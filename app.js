d8594c2d27a0f98473746c6d18003549bb7108b4
A. Ibrahim <ahmedmokhtar75@gmail.com>
2018-09-23 20:12:20 +0200

    Finished n-3-4

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ console.log('Starting app.js')
 
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
 const notes = require('./notes')
 
-const add = notes.add(3, 7)
-console.log(add)
+// console.log(_.isString(true))
+// console.log(_.isString('Ahmed'))
+
+const filteredArray = _.uniq(['Ahmed', 1, 2, 'Ahmed', 3, 1])
+console.log(filteredArray)
+
+// const add = notes.add(3, 7)
+// console.log(add)
 
 // let user = os.userInfo()
 
