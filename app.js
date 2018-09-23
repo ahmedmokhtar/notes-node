1251349f91b6b65a9855c0e5354db7c1dbea3e01
A. Ibrahim <ahmedmokhtar75@gmail.com>
2018-09-23 19:13:52 +0200

    Before challenge.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-console.log('Starting app.')
+console.log('Starting app.js')
 
 const fs = require('fs')
 const os = require('os')
+const notes = require('./notes')
 
-let user = os.userInfo()
 
-fs.appendFile('greeting.txt', `Hello ${user.username}!`, (error) => {
-    if (error) {
-        console.log('There was an error writing the file!')
-    }
-})
+
+// let user = os.userInfo()
+
+// fs.appendFile('greeting.txt', `Hello ${user.username}! Your age is ${notes.age}`, (error) => {
+//     if (error) {
+//         console.log('There was an error writing the file!')
+//     }
+// })
